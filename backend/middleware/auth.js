8a0c40501bb027c4e883253be17352c38bf935bb
shafwan khairi yusla <safwankhairiyusla@gmail.com>
2026-06-10 20:35:08 +0700

    middleware

---
+++ backend/middleware/auth.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1]; // Bearer TOKEN
+
+  if (!token) {
+    return res.status(401).json({ 
+      message: 'Akses ditolak. Token tidak ditemukan.' 
+    });
+  }
+
+  try {
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = verified;
+    next();
+  } catch (err) {
+    return res.status(403).json({ 
+      message: 'Token tidak valid atau sudah expired.' 
+    });
+  }
+};
+
+module.exports = authenticateToken;
